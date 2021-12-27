1ac91a162a5964fe79b81b4370ab0f44acad5bee
itsreallydrew <a.mclean2526@gmail.com>
2021-12-27 13:07:44 -0600

    changed public to external on owner functions, added safemath addition and multiplication, removed unnecessary initialization of state variables

--- contracts/CryptoArnies.sol
+++ contracts/CryptoArnies.sol
@@ -7,6 +7,10 @@ pragma solidity ^0.8.0;
 /********************************************* */
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/IAccessControl.sol";
+
 // import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 // import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 // import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
@@ -18,7 +22,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 // import "@openzeppelin/contracts/introspection/ERC165.sol";
 // import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 // import "@openzeppelin/contracts/utils/Address.sol";
-// import "@openzeppelin/contracts/utils/EnumerableSet.sol";
 // import "@openzeppelin/contracts/utils/Strings.sol";
 // import "@openzeppelin/contracts/security/Pausable.sol";
 
@@ -26,28 +29,35 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
-import "../OZ_Imports/ERC721Enumberable.sol";
+// import "../OZ_Imports/ERC721Enumberable.sol";
 
-contract CryptoArniez is ERC721Enumerable, ReentrancyGuard {
+contract CryptoArniez is
+    ERC721Enumerable,
+    ReentrancyGuard,
+    IAccessControl,
+    Ownable
+{
     using Counters for Counters.Counter;
     using SafeMath for uint256;
     using Address for address;
 
     string private baseURI;
     string private unrevealedURI;
+    string private baseExtension;
 
     Counters.Counter private _tokenIdCounter;
-    uint256 public totalMinted = 0;
+    uint256 public totalMinted;
     uint256 public TOTAL_SUPPLY = 5000;
     uint256 public price = 0.08 ether;
-    uint256 public maxMintAmount = 3;
+    uint256 public maxMintAmount = 5;
+    uint256 public RESERVED_ARNIES = 20;
 
     mapping(address => uint256) public whitelistAmount;
     mapping(address => mapping(uint256 => uint256)) public nftHolders;
 
     bool presaleLive;
     bool publicSaleLive;
-    bool revealed = false;
+    bool revealed;
     bool mintPaused = true;
 
     constructor() ERC721("CryptoArniez", "ARNIEZ") {}
@@ -56,33 +66,21 @@ contract CryptoArniez is ERC721Enumerable, ReentrancyGuard {
         return baseURI;
     }
 
-    function mintPresale(uint256 amount) public payable {
+    function mintPresale(uint256 amount) public payable nonReentrant {
         require(mintPaused == false);
-        require(amount <= 3 && amount > 0, "Please choose a valid amount");
-        whitelistAmount[msg.sender] = amount;
-        require(
-            whitelistAmount[msg.sender] > 0,
-            "You do not have any reserved mints"
-        );
-        // require(
-        //     amount <= whitelistAmount[msg.sender],
-        //     "You have a max of 2 mints"
-        // );
-        require(
-            totalMinted + amount < TOTAL_SUPPLY,
-            "Sale has ended. Please visit us on OpenSea"
-        );
-        require(msg.value == (price * amount), "Incorrect amount of ether");
+        require(amount <= 3 && amount > 0, "Invalid amount");
+        require(totalMinted + amount <= TOTAL_SUPPLY, "Sold out");
+        require(msg.value == price.mul(amount), "Incorrect amount of ether");
         for (uint256 i = 1; i <= amount; i++) {
             _tokenIdCounter.increment();
             uint256 tokenId = _tokenIdCounter.current();
             _safeMint(msg.sender, tokenId);
             tokenURI(tokenId);
-            totalMinted = totalMinted + 1;
+            totalMinted = totalMinted.add(1);
         }
     }
 
-    function mintPublic(uint256 amount) public payable {
+    function mintPublic(uint256 amount) public payable nonReentrant {
         require(mintPaused == false);
         require(amount <= 3, "Limit is 3 per wallet");
         require(
@@ -95,7 +93,7 @@ contract CryptoArniez is ERC721Enumerable, ReentrancyGuard {
             uint256 tokenId = _tokenIdCounter.current();
             _safeMint(msg.sender, tokenId);
             tokenURI(tokenId);
-            totalMinted = totalMinted + 1;
+            totalMinted = totalMinted.add(1);
         }
     }
 
@@ -103,41 +101,39 @@ contract CryptoArniez is ERC721Enumerable, ReentrancyGuard {
         return totalMinted;
     }
 
-
-
     // Only Owner Functions
-    function reveal() public onlyOwner {
+    function reveal() external onlyOwner {
         revealed = true;
     }
-    
-    function setPrice(uint256 _newPrice) public onlyOwner {
+
+    function setPrice(uint256 _newPrice) external onlyOwner {
         price = _newPrice;
     }
 
-    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+    function setmaxMintAmount(uint256 _newmaxMintAmount) external onlyOwner {
         maxMintAmount = _newmaxMintAmount;
     }
-    
-    function setUnrevealedURI(string memory _unrevealedURI) public onlyOwner {
+
+    function setUnrevealedURI(string memory _unrevealedURI) external onlyOwner {
         unrevealedURI = _unrevealedURI;
     }
 
-    function setBaseURI(string memory _newURI) public onlyOwner {
+    function setBaseURI(string memory _newURI) external onlyOwner {
         baseURI = _newURI;
     }
 
-    function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
         baseExtension = _newBaseExtension;
     }
 
-    function pause(bool _state) public onlyOwner {
+    function pause(bool _state) external onlyOwner {
         mintPaused = _state;
     }
 
-    function withdraw() public onlyOwner {
-        uint256 bal = address(this).bal;
-        payable(msg.sender).transfer(bal);
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
-
 }
-
